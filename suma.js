9ec1d9e010a1581506b6099ca82706fccb70d5c0
Henry809 <henryromerom530@gmail.com>
2024-10-04 23:16:45 -0400

    primer commit

---
+++ suma.js
@@ -0,0 +1,7 @@
+// suma.js
+
+function sumar(a, b) {
+    return a + b;
+}
+
+module.exports = sumar;
---
+++ suma.test.js
@@ -0,0 +1,11 @@
+// suma.test.js
+
+const sumar = require('./suma');
+
+test('suma de 2 y 3 debe ser 5', () => {
+    expect(sumar(2, 3)).toBe(5);
+});
+
+test('suma de -1 y 1 debe ser 0', () => {
+    expect(sumar(-1, 1)).toBe(0);
+});
